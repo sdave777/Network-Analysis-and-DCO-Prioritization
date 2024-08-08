a8bfa3c875f7cbaa21fbd567e766acfa4dafc1c9
David Scott <dave9860@gmail.com>
2024-08-08 13:19:52 -0600

    docker file paths

--- src/API.py
+++ src/API.py
@@ -3,15 +3,19 @@ from fastapi import FastAPI, Query, HTTPException
 from joblib import load
 import ipaddress
 from scipy.stats import ttest_ind
+import os
 
 # Initialize FastAPI app
 app = FastAPI()
 
+# Define the data directory
+data_dir = './data'
+
 # Load the trained model and label encoders
 try:
-    model = load('../data/random_forest_model.joblib')
-    proto_encoder = load('../data/proto_encoder.joblib')
-    conn_state_encoder = load('../data/conn_state_encoder.joblib')
+    model = load(os.path.join(data_dir, 'random_forest_model.joblib'))
+    proto_encoder = load(os.path.join(data_dir, 'proto_encoder.joblib'))
+    conn_state_encoder = load(os.path.join(data_dir, 'conn_state_encoder.joblib'))
 except FileNotFoundError:
     raise HTTPException(status_code=500, detail="Model or encoder files not found.")
 
@@ -72,7 +76,7 @@ def predict(ip: str = Query(..., description="Responding IP address")):
 def hypothesis_duration(threshold: float = Query(..., description="Duration threshold to distinguish long and short connections")):
     try:
         # Load the dataset
-        file_path = '../data/IOTNet24_IDS.csv'
+        file_path = os.path.join(data_dir, 'IOTNet24_IDS.csv')
         data = pd.read_csv(file_path)
 
         # Data preprocessing
@@ -125,7 +129,7 @@ def hypothesis_duration(threshold: float = Query(..., description="Duration thre
 def hypothesis_protocol(protocol: str = Query(..., description="Protocol to test (udp or tcp)")):
     try:
         # Load the dataset
-        file_path = '../data/IOTNet24_IDS.csv'
+        file_path = os.path.join(data_dir, 'IOTNet24_IDS.csv')
         data = pd.read_csv(file_path)
 
         # Data preprocessing
